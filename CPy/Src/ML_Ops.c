c2d7816495d61ce1df203995bf777d3d5fc210fd
Alt900 <darrenmarston34@gmail.com>
2024-09-02 13:22:08 -0400

    Sync desktop codespace

--- CPy/Src/ML_Ops.c
+++ CPy/Src/ML_Ops.c
@@ -1,38 +1,23 @@
-#include <stdio.h>
-
-long **A;
-int A_D1;
-int A_D2;
-
-long **B;
-int B_D1;
-int B_D2;
-
-//int AoB = transpose matrix A or matrix B
-void *Transpose(int AoB){//yeet return from call stack, who needs returns anyway
-    if (AoB==1){
-        for (int D1 = 0; D1 < A_D1; D1++){//
-            for (int D2 = 0; D2 < A_D2; D2++){
-                A[D1][D2]=A[D2][D1];
-            }
-        }
-    } else {
-        for (int D1 = 0; D1 < B_D1; D1++){
-            for (int D2 = 0; D2 < B_D2; D2++){
-                B[D1][D2] = B[D2][D1];
-            }
+long *FlattenMatrix(long **Matrix){
+    Array = (long*)malloc(row_count*size*sizeof(long));
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < size*row_count; i++){
+        Array[i]=Matrix[row][column];
+        column++;
+        if(i%size==0){
+            row++;
+            column=0;
         }
     }
+    return Array;
 }
 
-long **Matmul(long **Zeros){
-    for (int D1 = 0; D1<A_D1; D1++){
-        for (int D2 = 0; D2<B_D2; D2++){
-            Zeros[D1*A_D1+D2]=0;
-            for(int C1_R2 = 0; C1_R2 < A_D2; C1_R2++){
-                Zeros[D1*B_D2+D2]+=A[D1*A_D1+D2][D1*C1_R2+D2]*B[D1*A_D1+D2][C1_R2*B_D2+D2];
-            }
+long **Transpose(long **Matrix, int D1, int D2){
+    for (int i = 0; i < D1; i++){//
+        for (int j = 0; j < D2; j++){
+            Matrix[i][j]=Matrix[j][i];
         }
     }
-    return Zeros;
+    return Matrix;
 }
